5912e650d97be671966f439f03dc69f6628a6527
LucasDiello <lucasoliveiradiello@hotmail.com>
2024-09-18 17:04:43 -0300

    INITIAL COMMIT & feat: increment logic for task manager

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,120 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+export type Task = {
+  title: string;
+  completed: boolean;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [task, setTask] = useState("");
+
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+  const [showDeleteTaskModal, setShowDeleteTaskModal] = useState(false);
+
+  const handleToggleComplete = (index: number) => {
+    const updatedTasks = tasks.map((t, i) =>
+      i === index && { ...t, completed: !t.completed } 
+    );
+
+    setTasks(updatedTasks as Task[]);
+  };
+
+  return (
+    <div>
+      <h1>Bem-vindo de volta, Marcus</h1>
+      <h2>Segunda, 01 de dezembro de 2025</h2>
+
+      <div>
+        <h2>Suas tarefas de hoje</h2>
+        {tasks
+          .filter(task => !task.completed)
+          .map((task, index) => (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => handleToggleComplete(index)}
+              />
+              <span>{task.title}</span>
+              <button
+                onClick={() => {
+                  setShowDeleteTaskModal(true);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+
+        <h2>Tarefas finalizadas</h2>
+        {tasks
+          .filter(task => task.completed)
+          .map((task, index) => (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => handleToggleComplete(index)}
+              />
+              <span>{task.title}</span>
+              <button
+                onClick={() => {
+                  setShowDeleteTaskModal(true);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+            
+          ))
+            
+          }
+      </div>
+      <button onClick={() => setShowAddTaskModal(true)}>
+        Adicionar nova tarefa
+      </button>
+
+      {showAddTaskModal && (
+        <div className="modal">
+          <h2>Nova tarefa</h2>
+          <label htmlFor="">
+          Tarefa
+          <input
+            type="text"
+            placeholder="Título"
+            name="task"
+            onChange={(e) => setTask(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            </label>
+          <button onClick={() => setShowAddTaskModal(false)}>Cancelar</button>
+          <button
+            onClick={() => {
+              setShowAddTaskModal(false);
+              setTasks([...tasks, { title: task, completed: false }]);
+            }}
           >
-            Read our docs
-          </a>
+            Adicionar
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+
+      {showDeleteTaskModal && (
+        <div className="modal">  
+        <h2>Deletar tarefa</h2>  
+        <p>Tem certeza que você deseja deletar essa tarefa?</p>  
+        <button onClick={() => {
+          setShowDeleteTaskModal(false);
+        }}>Cancelar</button>  
+        <button onClick={() => {
+          setTasks(tasks.filter((task, index) => index !== 0));
+          setShowDeleteTaskModal(false);
+}}>Deletar</button>  
+      </div>  
+      )}
     </div>
   );
 }
